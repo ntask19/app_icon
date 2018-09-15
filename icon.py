b5e52dbd4a1cc4a51f970ec52998059979f6faa2
Task Nagashige <task.19.xx@gmail.com>
2018-09-15 22:40:46 +0900

    jsonからアイコンを切り出せるようにした

--- icon.py
+++ icon.py
@@ -1,5 +1,23 @@
 import json
+import glob
+from PIL import Image
 
 jsonDatas = open('icons.json', "r")
 iconDatas = json.loads(jsonDatas.read())
 jsonDatas.close()
+
+def getIconData(width):
+    for iconData in iconDatas.values():
+        if iconData['width'] == width:
+            return iconData
+
+
+files = glob.glob('input/*.png')
+
+for f in files:
+    img = Image.open(f)
+    iconData = getIconData(img.width)
+    for icon in iconData['contents']:
+        imgResize = img.resize((icon['size'], icon['size']))
+        imgResize.save('output/'+icon['name'])
+        print( icon['name'] )
